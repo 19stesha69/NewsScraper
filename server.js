d36dad002aa915d0bf38f36b4cf93f51c28215ce
Your Name <you@example.com>
2019-08-24 23:44:48 -0700

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const expressHandlebars = require("express-handlebars");
 
 // assigning PORT to the host's designated port OR 3000
@@ -24,6 +25,22 @@ app.set("view engine", "handlebars");
 // Every request will go through the router middleware
 app.use(router);
 
+// If deployed, use the deployed database OR use local mongoHead database
+const db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
+
+// Connect mongoose to the database
+mongoose.connect(db, {useNewUrlParser: true});
+// mongoose.connect(db, function(error) {
+//     // Log any errors connecting with mongoose
+//     if(error) {
+//         console.log(error);
+//     }
+//     // Or log a success message
+//     else {
+//         console.log("mongoose connection is successful");
+//     }
+// });
+
 // Listen on the port
 app.listen(PORT, function() {
     console.log("Listening on port:" + PORT);
@@ -39,8 +56,7 @@ app.listen(PORT, function() {
 // const axios = require("axios");
 // const cheerio = require("cheerio");
 
-// // Require alll of the database models
-// const db = require("./models");
+
 
 
 
