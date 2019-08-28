e70701ed88b08b86e79fcfad8722674f6456ed36
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-08-27 23:24:41 -0700

    create controller for the notes

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,11 @@
+
+// Bring in the scrape funtion from scripts directory
+const scrape = require("../scripts/scrape");
+
+// Bring articles and notes from the container
+const articlesController = require("../controllers/articles");
+const notesController = require("../controllers/notes");
+
 module.exports = function(router) {
     // This route renders the homepage
     router.get("/", function(req, res) {
